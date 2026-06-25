26f9a73bcfd795dac8e393ec11407846ece54ce9
limxj1985 <limxj.crc@gmail.com>
2026-06-25 16:02:27 +0800

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -1,9 +1,9 @@
 import { AgendaItem, NavItem } from './types';
 import React from 'react';
 
-export const CONFERENCE_DATE = "Wednesday, 30 September 2026"; [cite: 3]
-export const CONFERENCE_TIME = "0800 - 1300"; [cite: 4]
-export const CONFERENCE_VENUE = "Auditorium, Level 4, Ambulatory Care Centre, Hospital Raja Permaisuri Bainun"; [cite: 5]
+export const CONFERENCE_DATE = "Wednesday, 30 September 2026";
+export const CONFERENCE_TIME = "0800 - 1300";
+export const CONFERENCE_VENUE = "Auditorium, Level 4, Ambulatory Care Centre, Hospital Raja Permaisuri Bainun";
 export const REGISTRATION_LINK = "https://forms.gle/kh2gB8AaHnTpxi5b9";
 export const ABSTRACT_SUBMISSION_LINK = "https://forms.gle/5HjCutv7tKikJEKd9";
 export const ABSTRACT_TEMPLATE_LINK = "https://docs.google.com/document/d/1X22ZpyxWKCvzyL6RlIoYXSRloC3Sgu0usITxAgYQnH8/edit?tab=t.0";
@@ -25,77 +25,77 @@ export const AGENDA_DATA: AgendaItem[] = [
   { 
     time: "07:30 - 08:00", 
     title: "Registration", 
-    location: "ACC, Level 4", [cite: 2]
+    location: "ACC, Level 4",
     type: "general" 
   },
   { 
     time: "08:00 - 09:00", 
     title: "Opening Ceremony", 
-    location: "Auditorium", [cite: 2]
+    location: "Auditorium",
     type: "general",
     details: [
-      "National Anthem 'Negaraku', HRPB & KKM Songs", [cite: 2]
-      "Doa Recital", [cite: 2]
-      "Welcoming Speech by Dr. Fakhruddin Bin Amran (Director of HRPB)", [cite: 2]
-      "Officiating Speech by Dato' Dr. Feisul Idzwan Mustapha (Director of JKN Perak)", [cite: 2]
-      "PRC 2026 Officiating Launch Gimmick", [cite: 2]
-      "Photography Session" [cite: 2]
+      "National Anthem 'Negaraku', HRPB & KKM Songs",
+      "Doa Recital",
+      "Welcoming Speech by Dr. Fakhruddin Bin Amran (Director of HRPB)",
+      "Officiating Speech by Dato' Dr. Feisul Idzwan Mustapha (Director of JKN Perak)",
+      "PRC 2026 Officiating Launch Gimmick",
+      "Photography Session"
     ]
   },
   { 
     time: "09:00 - 09:20", 
     title: "Morning Tea Break / Booth & Poster Exhibition", 
-    location: "Auditorium / Foyer Level 4", [cite: 2]
+    location: "Auditorium / Foyer Level 4",
     type: "break" 
   },
   { 
     time: "09:20 - 10:20", 
     title: "Plenary Sessions", 
-    location: "Auditorium", [cite: 2]
+    location: "Auditorium",
     type: "session",
     details: [
-      "Plenary 1: Prof. Dr. Rukman Bin Awang Hamat", [cite: 2]
-      "Plenary 2: Prof. Datin Dr. Sarinder Kaur Dhillon", [cite: 2]
-      "Plenary 3: Ms. Asha Thanabalan" [cite: 2]
+      "Plenary 1: Prof. Dr. Rukman Bin Awang Hamat",
+      "Plenary 2: Prof. Datin Dr. Sarinder Kaur Dhillon",
+      "Plenary 3: Ms. Asha Thanabalan"
     ]
   },
   { 
     time: "10:20 - 11:35", 
     title: "Oral Presentations 1-5 & Concurrent Poster Evaluation", 
-    location: "Auditorium / Foyer Level 4", [cite: 2]
+    location: "Auditorium / Foyer Level 4",
     type: "session" 
   },
   { 
     time: "11:35 - 12:00", 
     title: "CRC Research Achievements 2026", 
-    location: "Auditorium", [cite: 2]
+    location: "Auditorium",
     type: "session",
     details: [
-      "Presentation 1: CRC HRPB", [cite: 2]
-      "Presentation 2: CRC Manjung", [cite: 2]
-      "Presentation 3: CRC Taiping" [cite: 2]
+      "Presentation 1: CRC HRPB",
+      "Presentation 2: CRC Manjung",
+      "Presentation 3: CRC Taiping"
     ]
   },
   { 
     time: "12:00 - 12:45", 
     title: "Prize Giving Ceremony", 
-    location: "Auditorium", [cite: 2]
+    location: "Auditorium",
     type: "general",
     details: [
-      "Chief Judge's Review", [cite: 2]
-      "Presentation of Appreciation Tokens (Invited Speakers & Judges)", [cite: 2]
-      "Oral Presentation Awards", [cite: 2]
-      "Poster Presentation Awards", [cite: 2]
-      "CRC Research Award 2026" [cite: 2]
+      "Chief Judge's Review",
+      "Presentation of Appreciation Tokens (Invited Speakers & Judges)",
+      "Oral Presentation Awards",
+      "Poster Presentation Awards",
+      "CRC Research Award 2026"
     ]
   },
   { 
     time: "12:45 - 13:00", 
     title: "Closing Remarks", 
-    location: "Auditorium", [cite: 2]
+    location: "Auditorium",
     type: "general",
     details: [
-      "Closing Speech by Dato' Dr. Philip Rajan Devesahayam (Organizing Chairman)" [cite: 2]
+      "Closing Speech by Dato' Dr. Philip Rajan Devesahayam (Organizing Chairman)"
     ]
   },
 ];
