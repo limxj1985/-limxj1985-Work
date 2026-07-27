eabc65debeb04e023eb32ee40799cb536b4e221d
limxj1985 <limxj.crc@gmail.com>
2026-07-27 14:56:54 +0800

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const CONFERENCE_DATE = "Wednesday, 30 September 2026";
 export const CONFERENCE_TIME = "0800 - 1300";
-export const CONFERENCE_VENUE = "Auditorium, Level 4, Ambulatory Care Centre, Hospital Raja Permaisuri Bainun";
+export const CONFERENCE_VENUE = "Auditorium, Level 4, Ambulatory Care Centre (ACC), Hospital Raja Permaisuri Bainun, Jalan Raja Ashman Shah, 30450 Ipoh, Perak";
 export const REGISTRATION_LINK = "https://forms.gle/kh2gB8AaHnTpxi5b9";
 export const ABSTRACT_SUBMISSION_LINK = "https://forms.gle/5HjCutv7tKikJEKd9";
 export const ABSTRACT_TEMPLATE_LINK = "https://docs.google.com/document/d/1X22ZpyxWKCvzyL6RlIoYXSRloC3Sgu0usITxAgYQnH8/edit?tab=t.0";
@@ -34,17 +34,17 @@ export const AGENDA_DATA: AgendaItem[] = [
     location: "Auditorium",
     type: "general",
     details: [
-      "National Anthem 'Negaraku', HRPB & KKM Songs",
+      "National Anthem 'Negaraku', KKM & HRPB Songs",
       "Doa Recital",
       "Welcoming Speech by Dr. Fakhruddin Bin Amran (Director of HRPB)",
-      "Officiating Speech by Dato' Dr. Feisul Idzwan Mustapha (Director of JKN Perak)",
+      "Officiating Speech by Dato' Dr. Feisul Idzwan bin Dato' Mustapha (Director of JKN Perak)",
       "PRC 2026 Officiating Launch Gimmick",
       "Photography Session"
     ]
   },
   { 
     time: "09:00 - 09:20", 
-    title: "Morning Tea Break / Poster Exhibition", 
+    title: "Morning Tea Break / e-Poster Exhibition Session", 
     location: "Auditorium / Foyer Level 4",
     type: "break" 
   },
@@ -54,20 +54,20 @@ export const AGENDA_DATA: AgendaItem[] = [
     location: "Auditorium",
     type: "session",
     details: [
-      "Plenary 1: Prof. Dr. Rukman Bin Awang Hamat",
-      "Plenary 2: Prof. Datin Dr. Sarinder Kaur Dhillon",
-      "Plenary 3: Ms. Asha Thanabalan"
+      "Plenary 1: Prof. Dr. Rukman Bin Awang Hamat (Universiti Putra Malaysia)",
+      "Plenary 2: Prof. Datin Dr. Sarinder Kaur Dhillon (Universiti Malaya)",
+      "Plenary 3: Dr. Akhmal Yusof (Clinical Research Malaysia)"
     ]
   },
   { 
     time: "10:20 - 11:35", 
-    title: "Oral Presentations (5)  & Concurrent Poster Evaluation", 
+    title: "Oral Presentations (1-5) & Concurrent Poster Evaluation", 
     location: "Auditorium / Foyer Level 4",
     type: "session" 
   },
   { 
     time: "11:35 - 12:00", 
-    title: "CRC Research Presentations 2026", 
+    title: "CRC Research Achievements 2026", 
     location: "Auditorium",
     type: "session",
     details: [
