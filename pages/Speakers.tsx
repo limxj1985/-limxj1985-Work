c12e1b2afdc773bbab5c71a7a5661bd39ed07e6d
limxj1985 <limxj.crc@gmail.com>
2026-09-25 16:03:36 +0800

    Update Speakers.tsx

--- pages/Speakers.tsx
+++ pages/Speakers.tsx
@@ -3,7 +3,7 @@ import { Mic2, User } from 'lucide-react';
 
 // Importing local images from your project structure
 import RukmanPhoto from '../components/assets/69017.jpg';
-import AshaPhoto from '../components/assets/69018.jpg';
+import AkhmalPhoto from '../components/assets/69018.jpg';
 import SarinderPhoto from '../components/assets/Screenshot 2026-06-25 161644.png';
 
 interface Speaker {
@@ -27,7 +27,7 @@ const Speakers: React.FC = () => {
     {
       name: "DR. AKHMAL YUSOF",
       role: "Plenary 3 Speaker",
-      image: AshaPhoto, 
+      image: AkhmalPhoto, 
     },
   ];
 
